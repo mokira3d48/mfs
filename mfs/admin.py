8bcb17e6919f8c14bc8a70d291253504f31da54b
Dr Mokira <arnoldmokira3d48@gamil.com>
2022-10-05 10:26:17 -0800

    Model Admin is customized for a MFS file model.

--- mfs/admin.py
+++ mfs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class FileModelAdmin(admin.ModelAdmin):
+    """
+    Modèle administratif des fichiers du système 
+    de fichier du serveur.
+    """
+    list_display = ['name', 'ext', 'size', 'visibility', 'filedir'];
+    search_fields = ['name', 'ext'];
+    list_filter   = ['visibility', 'filedir'];
+    ordering      = ['created_at'];
+    fields        = [('name', 'ext'), 'filedir', 'visibility'];
+
